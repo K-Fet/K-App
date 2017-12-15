eaaa92abc3ef5d6a5b37702cc05268c74fc376ae
Marc-Antoine Fernandes <embraser01@gmail.com>
2017-12-15 14:57:07 +0100

    Fix Sequelize Mocking bug Tweaks for PULL REQUEST TEMPLATE

--- server/test/utils/mock-database.js
+++ server/test/utils/mock-database.js
@@ -1,2 +1,12 @@
-// TODO Complete database object for more advanced mock
+const { DataTypes } = require('sequelize');
 
+module.exports = {
+    fixDataTypes(sequelizeInstance) {
+        // Fix UUID bug, see https://github.com/rochejul/sequelize-mocking/issues/16
+        sequelizeInstance.modelManager.all.forEach(model => {
+            for (const att of Object.values(model.attributes)) {
+                att.type = new DataTypes[att.type.key]();
+            }
+        });
+    }
+};
