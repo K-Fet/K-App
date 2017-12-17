9856ceb5f387e1c1e22f2abc72822c9e25e87914
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2017-12-17 19:41:57 +0100

    Début du travail

--- server/app/services/barman-service.js
+++ server/app/services/barman-service.js
@@ -1,5 +1,6 @@
 const logger = require('../../logger');
 const { Barman } = require('../models/barman');
+const { Service } = require ('../models/service');
 
 /**
  * Return all barmen of the app.
@@ -12,6 +13,74 @@ async function getAllBarmen() {
     return await Barman.findAll();
 }
 
+/**
+ * Return all barmen of the app.
+ *
+ * @returns {Promise<Array>} Barmen
+ */
+async function createBarman() { // quels sont les attributs à changer
+
+    logger.info('Barman service: create a barman');
+}
+
+/**
+ * Return all barmen of the app.
+ *
+ * @returns {Promise<Array>} Barmen
+ */
+async function getBarmanById(barmanId) {
+
+    logger.info('Barman service: get a barman by his Id');
+    return await Barman.findById(barmanId);
+}
+
+/**
+ * Return all barmen of the app.
+ *
+ * @returns void
+ */
+async function deleteBarmanById(barmanId) {
+
+    logger.info('Barman service: delete a barman by his Id');
+    Barman.destroy({
+        where :{Id: barmanId}
+    });
+}
+
+/**
+ * Return all barmen of the app.
+ *
+ * @returns {Promise<Array>} Barmen
+ */
+async function updateBarmanById(barmanId) { // quels sont les attributs à changer
+
+    logger.info('Barman service: update a barman by his Id');
+    Barman.update({
+
+    },
+    { where :{Id: barmanId}
+
+    });
+}
+
+/**
+ * Return all barmen of the app.
+ *
+ * @returns {Promise<Array>} Barmen
+ */
+async function getServicesOfBarman(barmanId) {
+    logger.info('Barman service: get services of a barman');
+    return await Service.findAll({
+        where:{Barman: barmanId}//faux à corriger
+    });
+}
+
+
 module.exports = {
-    getAllBarmen
+    getAllBarmen,
+    createBarman,
+    getBarmanById,
+    deleteBarmanById,
+    updateBarmanById,
+    getServicesOfBarman
 };
