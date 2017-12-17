7c55ba74910b9448e5561a029336ff5d1d0e9b80
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2017-12-17 19:41:57 +0100

    Début du travail

--- server/app/controllers/barman-controller.js
+++ server/app/controllers/barman-controller.js
@@ -1,3 +1,4 @@
+const logger = require('../../logger');
 const barmanService = require('../services/barman-service');
 
 /**
@@ -8,11 +9,80 @@ const barmanService = require('../services/barman-service');
  * @return {Promise.<void>} Nothing
  */
 async function getAllBarmen(req, res) {
-    const barmen = await barmanService.getAllBarmen();
+    try {
+        const barmen = await barmanService.getAllBarmen();
 
-    res.json(barmen);
+        res.json(barmen);
+    } catch(e) {
+        logger.error('Error while getting all barmen', e),
+        res.sendStatus(500);
+    }
+
+    return res.end();
+}
+
+/**
+ * Fetch the barman linked to an Id from the database.
+ *
+ * @param req Request
+ * @param res Response
+ * @return {Promise.<void>} Nothing
+ */
+async function getBarmanById(req, res) {
+    try {
+        const barmanId = req.params.barmanId;
+        const barman = await barmanService.getBarmanById(barmanId);
+
+        res.json(barman);
+    }catch (e) {
+        logger.error('Error while getting a barman by his Id', e);
+        res.sendStatus(500);
+    }
+
+    return res.end();
+}
+
+/**
+ * Delete the barman linked to an Id from the database.
+ *
+ * @param req Request
+ * @param res Response
+ * @return {Promise.<void>} Nothing
+ */
+async function deleteBarmanById(req, res) {
+    try {
+        const barmanId = req.params.barmanId;
+        await barmanService.deleteBarmanById(barmanId);
+
+    } catch (e) {
+        logger.error('Error while deleting a barman by his Id', e);
+        res.sendStatus(500);
+    }
+}
+
+/**
+ * Fetch the services of a barman linked to an Id from the database.
+ *
+ * @param req Request
+ * @param res Response
+ * @return {Promise.<void>} Nothing
+ */
+async function getServicesBarman(req, res) {
+    try {
+        const barmanId = req.params.barmanId;
+        const services = await barmanService.getServicesOfBarman(barmanId);
+
+        res.json(services);
+    } catch (e) {
+        logger.error('Error while getting Services of a barman');
+        res.sendStatus(500);
+    }
+    return res.end();
 }
 
 module.exports = {
     getAllBarmen,
+    getBarmanById,
+    deleteBarmanById,
+    getServicesBarman
 };
--- server/app/routes/barmen.js
+++ server/app/routes/barmen.js
@@ -3,5 +3,8 @@ const guard = require('express-jwt-permissions')();
 const barmanController = require('../controllers/barman-controller');
 
 router.get('/', guard.check('barman:read'), barmanController.getAllBarmen);
+router.get('/:barmanId(\\d+)', guard.check('barman:read'), barmanController.getBarmanById);
+router.delete('/:barmanId(\\d+)', guard.check('barman:write'), barmanController.deleteBarmanById);
+router.get('/:barmanId(\\d+)/services', guard.check('barma,:read', 'service:read'), barmanController.getServicesBarman);
 
 module.exports = router;
