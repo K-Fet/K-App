455ee7f754c2d57755ce66fa1813e41d0654b400
Marc-Antoine FERNANDES <embraser01@gmail.com>
2017-12-19 15:29:17 +0100

    Add refresh JWT option.

--- server/app/routes/auth.js
+++ server/app/routes/auth.js
@@ -7,6 +7,7 @@ router.post('/login', am(authController.login));
 // Add API specific middleware
 router.use(require('../middlewares/auth-guard'));
 
+router.get('/refresh', am(authController.refresh));
 router.get('/logout', am(authController.logout));
 
 module.exports = router;
--- server/app/services/auth-service.js
+++ server/app/services/auth-service.js
@@ -2,7 +2,7 @@ const logger = require('../../logger');
 const jwt = require('jsonwebtoken');
 const uuidv4 = require('uuid/v4');
 
-const { jwtSecret } = require('../../config/jwt');
+const { jwtSecret, expirationDuration } = require('../../config/jwt');
 const { verify, createUserError } = require('../../utils');
 
 const { ConnectionInformation, JWT } = require('../models');
@@ -37,6 +37,37 @@ async function login(username, password) {
     if (!user || !verify(password, user.password)) {
         throw createUserError('LoginError', 'Bad username/password combination');
     }
+
+    return await createJWT(user);
+}
+
+/**
+ * Refresh a token and revoked the old one.
+ *
+ * @param currentTokenId {String} Current token id
+ * @returns {Promise<String>} The newly created token
+ */
+async function refresh(currentTokenId) {
+    // Revoke old token
+    const token = await logout(currentTokenId);
+
+    // Get user to create a new token.
+    // Here we don't really need transaction
+    // because current token is already revoked
+    const user = token.getConnection();
+
+    return await createJWT(user);
+}
+
+
+/**
+ * Create a new JWT including permissions.
+ *
+ * @param user {ConnectionInformation} User
+ * @returns {Promise<String>} Return a JWT.
+ */
+async function createJWT(user) {
+
     // Sign with default (HMAC SHA256)
     const id = uuidv4();
 
@@ -50,32 +81,37 @@ async function login(username, password) {
         'member:read'
     ];
 
-    logger.info(`Logging user ${user.id}, has permissions : ${permissions.join(', ')}`);
+    logger.info(`Creating a new JWT ${user.id}, with permissions : ${permissions.join(', ')}`);
 
     return jwt.sign({
         jit: id,
+        exp: Math.floor(Date.now() / 1000) + (60 * 60 * expirationDuration),
         permissions
     }, jwtSecret);
 }
+
 
 /**
  * Logout a member by revoking his token.
  *
  * @param tokenId Token's JIT
- * @return {Promise.<void>} Nothing
+ * @return {Promise.<JWT>} The deleted token
  * @throws An error if the token could not be find.
  */
 async function logout(tokenId) {
     const token = await JWT.findById(tokenId);
-    
+
     if (!token) throw createUserError('LogoutError', 'This token does not exist');
 
     await token.update({ revoked: true });
+
+    return token;
 }
 
 
 module.exports = {
     isTokenRevoked,
     login,
+    refresh,
     logout
 };
