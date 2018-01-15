62504ffc58290773279f6c6f9b757280774655e1
mickaelgadoud <mickaelgadoud@gmail.com>
2018-01-15 12:56:57 +0100

    reviewed Services

--- server/app/routes/services.js
+++ server/app/routes/services.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const guard = require('express-jwt-permissions')();
 const am = require('../../utils/async-middleware');
 const serviceController = require('../controllers/service-controller');
 
-router.get('/', guard.check('service:read'), am(serviceController.getAllServices));
-
+router.get('/', am(serviceController.getAllServices));
 router.post('/', am(serviceController.createService));
-
 router.get('/:id(\\d+)', am(serviceController.getServiceById));
 router.put('/:id(\\d+)', am(serviceController.updateService));
 router.delete('/:id(\\d+)', am(serviceController.deleteService));
