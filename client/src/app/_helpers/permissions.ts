c012997921e94015749fe0049fc6a25689881462
Corentin Giraud <corentin.giraud@insa-lyon.fr>
2018-03-04 14:27:09 +0100

    Intial commit, install lib + initialize permissions

---
+++ client/src/app/_helpers/permissions.ts
@@ -0,0 +1,14 @@
+export const permissions = [
+    'barman:read',
+    'barman:write',
+    'role:read',
+    'role:write',
+    'kommission:read',
+    'kommission:write',
+    'member:read',
+    'member:write',
+    'member:read',
+    'member:write',
+    'service:read',
+    'service:write',
+];
