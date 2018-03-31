0692c34542fd41b016b56e70303bd3095f9ffa11
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2018-03-31 20:11:14 +0200

    Fix Issue about the choice of a godfather Front & Back

--- client/src/app/barmen/edit/barman-edit.component.ts
+++ client/src/app/barmen/edit/barman-edit.component.ts
@@ -99,7 +99,14 @@ export class BarmanEditComponent implements OnInit {
         // Get barmen
         this.barmanService.getAll().subscribe(barmen => {
             this.barmen = barmen;
-        });
+            let i = 0
+            let barman = this.barmen[0];
+            while (this.barmen[i].id !== this.currentBarman.id){
+                i++;
+                barman = barmen[i];
+            }
+            this.barmen.splice(i,1);
+        })
 
         // Get connected user
         this.authService.$currentUser.subscribe((user: ConnectedUser) => {
