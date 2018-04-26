52edf2cd2d883022e078d2498b0bb11cf527070f
BaptisteViry <baptiste.viry@orange.fr>
2018-04-26 21:18:29 +0200

    refonte du new avec des formGroups

--- client/src/app/templates/new/templates-new.component.ts
+++ client/src/app/templates/new/templates-new.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Template } from '../../_models/index';
+import { FormArray, FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+//import { Template } from '../../_models/index';
 
 @Component({
     templateUrl: './templates-new.component.html',
 })
 
 export class TemplateNewComponent {
+   /*
     name: String;
     template: Template = new Template();
 
@@ -28,10 +29,11 @@ export class TemplateNewComponent {
             minutes: Number
         }
     }>;
-
-    serviceForm: FormGroup;
-    templateForm: FormGroup;
-    formArray: FormArray;
+*/
+    templateNameFormGroup: FormGroup;
+    servicesFormArray: FormArray;
+    generalFormArray: FormArray;
+    generalFormGroup: FormGroup;
 
     WEEK_DAY = [
         { id: '1', value: 'Lundi' },
@@ -42,45 +44,76 @@ export class TemplateNewComponent {
         { id: '6', value: 'Samedi' },
         { id: '7', value: 'Dimanche' } ];
 
-    
-
     constructor(
         private fb: FormBuilder
     ) {
-        this.createForm();
+        this.createForms();
     }
 
-    createForm(): void {
-        this.serviceForm = this.fb.group({
-            startFormControl: new FormControl('', [Validators.required]),
-            endFormControl: new FormControl('', [Validators.required]),
-            dayFormControl: new FormControl('', [Validators.required]),
-            nbMaxFormControl: new FormControl('', [Validators.required]),
+    createForms(): void {
+        this.templateNameFormGroup = this.fb.group({
+            templateNameFormControl: ['', [Validators.required]],
         });
-        this.formArray = this.fb.array([
-            this.serviceForm,
+        this.servicesFormArray = this.fb.array([]);
+        this.generalFormArray = this.fb.array([
+            this.templateNameFormGroup,
+            this.servicesFormArray,
         ]);
-        this.templateForm = this.fb.group({
-            nameFormControl: new FormControl('', [Validators.required]),
-            formArray: this.formArray,
+        this.generalFormGroup = this.fb.group({
+            generalFormArray: this.generalFormArray,
+        });
+
+    }
+
+    addServiceForm(nbMax: Number, startAt: Date, endAt: Date, startDay: Number, endDay: Number): void {
+        const serviceFormGroup = this.fb.group({
+            startFormControl: [startAt, Validators.required],
+            startDayFormControl: [startDay, Validators.required],
+            endFormControl: [endAt, Validators.required],
+            endDayFormControl: [endDay, Validators.required],
+            nbMaxFormControl: [nbMax, Validators.required],
+        });
+        serviceFormGroup.valueChanges.subscribe(() => {
+            this.sortServiceForm();
+        });
+        this.servicesFormArray.push(serviceFormGroup);
+    }
+
+    sortServiceForm(): void {
+        this.servicesFormArray.controls.sort((a, b) => {
+            const aStartAt = (a as FormGroup).controls.startAtFormControl.value;
+            const bStartAt = (b as FormGroup).controls.startAtFormControl.value;
+            if (aStartAt < bStartAt) {
+                return -1;
+            } else if (aStartAt > bStartAt) {
+                return 1;
+            }
+            return 0;
         });
     }
 
-    toNumber(date: Date, selectedDay): { day: Number, hours: Number, minutes: Number } {
+    getControls(): Array<AbstractControl> {
+        return (this.generalFormArray.get([1]) as FormArray).controls;
+    }
+
+    removeServiceForm(fromGroupId: Number): void {
+        this.servicesFormArray.removeAt(+fromGroupId);
+    }
+
+
+   /* toNumber(date: Date, selectedDay): { day: Number, hours: Number, minutes: Number } {
         return {
             day: selectedDay,
             hours: date.getHours(),
             minutes: date.getMinutes(),
         };
-    }
+    }*/
 
-    addService(): void {
+  /*  addService(): void {
         const val = {
             nbMax: this.nbMax,
             startAt: this.toNumber(this.startAt, this.selectedDay),
             endAt: this.toNumber(this.endAt, this.selectedDay),
         };
-        console.log(this.services.push(val));
-        console.log("coucou");
-    }
+    } */
 }
