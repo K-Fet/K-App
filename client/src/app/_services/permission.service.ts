7d3b9a5e92bfffdcd05a258935bc863d43dd3116
Marc-Antoine Fernandes <embraser01@gmail.com>
2018-04-27 16:54:39 +0200

    [tslint]: Add space in ES6 imports

--- client/src/app/_services/permission.service.ts
+++ client/src/app/_services/permission.service.ts
@@ -1,18 +1,17 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Permission} from '../_models';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Permission } from '../_models';
 
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
-import {Observable} from 'rxjs';
-import {NgxPermissionsService} from 'ngx-permissions';
+import { Observable } from 'rxjs';
+import { NgxPermissionsService } from 'ngx-permissions';
 
 @Injectable()
 export class PermissionService {
 
     constructor(private http: HttpClient,
-                private ngxPermissions: NgxPermissionsService) {
-    }
+                private ngxPermissions: NgxPermissionsService) {}
 
     getAll(): Observable<Array<Permission>> {
         return this.http.get<Array<Permission>>('/api/permissions').concatMap(perms => {
