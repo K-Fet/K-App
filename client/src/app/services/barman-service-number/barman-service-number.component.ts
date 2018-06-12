5ef5db84c1f8dd9125b01405d635657f1ad4a303
Corentin GIRAUD <corentin.giraud@insa-lyon.fr>
2018-06-12 15:44:44 +0200

    Fix lint errors

--- client/src/app/services/barman-service-number/barman-service-number.component.ts
+++ client/src/app/services/barman-service-number/barman-service-number.component.ts
@@ -4,32 +4,32 @@ import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { BarmanService, ServiceService } from '../../_services';
 
 @Component({
-    selector: 'app-barman-service-number',
-    templateUrl: './barman-service-number.component.html',
+  selector: 'app-barman-service-number',
+  templateUrl: './barman-service-number.component.html',
 })
 export class BarmanServiceNumberComponent implements OnInit {
 
-    displayedColumns = ['nickname', 'number'];
-    barmenData: MatTableDataSource<Barman>;
+  displayedColumns = ['nickname', 'number'];
+  barmenData: MatTableDataSource<Barman>;
 
-    @ViewChild(MatSort) sort: MatSort;
-    @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
-    constructor(private barmanService: BarmanService, private serviceService: ServiceService) { }
+  constructor(private barmanService: BarmanService, private serviceService: ServiceService) { }
 
-    ngOnInit(): void {
-        this.serviceService.getWeek().subscribe(week => {
-            this.barmanService.getAll().subscribe(barmen => {
-                barmen.filter(barman => barman.active).map(barman => {
-                    this.barmanService.getServices(barman.id, week.start, week.end).subscribe(services => {
-                        barman.services = services;
-                    });
-                    return barman;
-                });
-                this.barmenData = new MatTableDataSource(barmen);
-                this.barmenData.paginator = this.paginator;
-                this.barmenData.sort = this.sort;
-            });
+  ngOnInit(): void {
+    this.serviceService.getWeek().subscribe((week) => {
+      this.barmanService.getAll().subscribe((barmen) => {
+        barmen.filter(barman => barman.active).map((barman) => {
+          this.barmanService.getServices(barman.id, week.start, week.end).subscribe((services) => {
+            barman.services = services;
+          });
+          return barman;
         });
-    }
+        this.barmenData = new MatTableDataSource(barmen);
+        this.barmenData.paginator = this.paginator;
+        this.barmenData.sort = this.sort;
+      });
+    });
+  }
 }
