be6b7bc32c0b408e14ce5205c2d1530b40f83fef
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2018-06-17 20:04:55 +0200

    début du task

--- client/src/app/kommissions/view/kommission-view.component.ts
+++ client/src/app/kommissions/view/kommission-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import { TaskService } from '../../_services';
-//import { ActivatedRoute, Router } from '@angular/router';
+import { TaskService } from '../../_services';
+import { ActivatedRoute/*, Router */} from '@angular/router';
 import { Task } from '../../_models';
 
 @Component({
@@ -12,12 +12,19 @@ export class KommissionViewComponent implements OnInit {
   task: Task = new Task();
 
   constructor(
-    /*private taskService: TaskService,
-    private toasterService: ToasterService,
+    private taskService: TaskService,
+    //private toasterService: ToasterService,
     private route: ActivatedRoute,
-    private router: Router*/
+    //private router: Router
   ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.task.id = params['id'];
+      this.taskService.getById(+this.task.id).subscribe((task) => {
+        this.task = task;
+        console.log(task);
+      });
+    });
   }
 }
