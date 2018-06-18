92cbf07d2ab1e2b066c82dab870d43da311654fc
Corentin GIRAUD <corentin.giraud@insa-lyon.fr>
2018-06-18 21:38:41 +0200

    Fix test

--- server/config/contactForm.js
+++ server/config/contactForm.js
@@ -1,8 +1,8 @@
 module.exports = {
-  CONCERT_MAIL: process.env.CONTACT_CONCERT_MAIL.split(',') || 'contact@fake.com',
-  EVENT_MAIL: process.env.CONTACT_EVENT_MAIL.split(',') || 'contact@fake.com',
-  LOST_MAIL: process.env.CONTACT_LOST_MAIL.split(',') || 'contact@fake.com',
-  WEBSITE_MAIL: process.env.CONTACT_WEBSITE_MAIL.split(',') || 'contact@fake.com',
+  CONCERT_MAIL: process.env.CONTACT_CONCERT_MAIL ? process.env.CONTACT_CONCERT_MAIL.split(',') : 'contact@fake.com',
+  EVENT_MAIL: process.env.CONTACT_EVENT_MAIL ? process.env.CONTACT_EVENT_MAIL.split(',') : 'contact@fake.com',
+  LOST_MAIL: process.env.CONTACT_LOST_MAIL ? process.env.CONTACT_LOST_MAIL.split(',') : 'contact@fake.com',
+  WEBSITE_MAIL: process.env.CONTACT_WEBSITE_MAIL ? process.env.CONTACT_WEBSITE_MAIL.split(',') : 'contact@fake.com',
   RECAPTCHA: {
     SECRET: process.env.RECAPTCHA_SECRET || '6LfIS1gUAAAAAKFZgZm1ZOVhzEFDMrD8GcV2W2X2',
   },
