593f0adc13939e835a4828cff17d9ec6f09bf34b
Corentin GIRAUD <corentin.giraud@insa-lyon.fr>
2018-06-20 21:44:07 +0200

    Make the version dynamic through env

--- client/src/app/footer/footer.component.ts
+++ client/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  version: String = environment.version;
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
