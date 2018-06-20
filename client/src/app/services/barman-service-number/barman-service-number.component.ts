a5afeaf181f3e0b1a349676787dac040124aebbc
Corentin GIRAUD <corentin.giraud@insa-lyon.fr>
2018-06-20 12:07:36 +0200

    Fix sort issue

--- client/src/app/services/barman-service-number/barman-service-number.component.ts
+++ client/src/app/services/barman-service-number/barman-service-number.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Barman } from '../../_models';
+import { Service } from '../../_models';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import { BarmanService, ServiceService } from '../../_services';
+import { forkJoin, Observable } from 'rxjs';
+
+interface barmanServiceData {
+  name: String;
+  services: Number;
+}
 
 @Component({
   selector: 'app-barman-service-number',
@@ -9,26 +15,37 @@ import { BarmanService, ServiceService } from '../../_services';
 })
 export class BarmanServiceNumberComponent implements OnInit {
 
-  displayedColumns = ['nickname', 'number'];
-  barmenData: MatTableDataSource<Barman>;
+  displayedColumns = ['name', 'services'];
+  barmenData: MatTableDataSource<barmanServiceData> = new MatTableDataSource();
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  ngAfterViewInit() {
+    this.barmenData.paginator = this.paginator;
+    this.barmenData.sort = this.sort;
+  }
+
   constructor(private barmanService: BarmanService, private serviceService: ServiceService) { }
 
   ngOnInit(): void {
-    this.serviceService.getWeek().subscribe((week) => {
-      this.barmanService.getAll().subscribe((barmen) => {
-        barmen.filter(barman => barman.active).map((barman) => {
-          this.barmanService.getServices(barman.id, week.start, week.end).subscribe((services) => {
-            barman.services = services;
-          });
-          return barman;
+    let barmanDataTable: barmanServiceData[] = [];
+    this.barmanService.getAll().subscribe((barmen) => {
+      this.serviceService.getWeek().subscribe((week) => {
+        const services$: Observable<Service[]>[] = [];
+        barmen.filter(barman => barman.active).forEach((barman) => {
+          services$.push(this.barmanService.getServices(barman.id, week.start, week.end));
         });
-        this.barmenData = new MatTableDataSource(barmen);
-        this.barmenData.paginator = this.paginator;
-        this.barmenData.sort = this.sort;
+        forkJoin(services$).subscribe((services) => {
+          barmanDataTable = [];
+          barmen.forEach((barman) => {
+            barmanDataTable.push({
+              name: barman.nickname,
+              services: services[barmen.indexOf(barman)].length,
+            });
+          });
+          this.barmenData.data = barmanDataTable;
+        });
       });
     });
   }
