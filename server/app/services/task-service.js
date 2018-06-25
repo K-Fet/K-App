94879d6f574cdbe4295723fd5b6f08c9790c649c
Corentin GIRAUD <corentin.giraud@insa-lyon.fr>
2018-06-25 08:47:50 +0200

    Implement review comments

--- server/app/services/task-service.js
+++ server/app/services/task-service.js
@@ -113,7 +113,7 @@ async function updateTask(taskId, updatedTask, _embedded) {
   }
 
   // Associations
-  await setAssociations(_embedded, currentTask, null, transaction, false);
+  await setAssociations(_embedded, currentTask, null, transaction);
 
   await transaction.commit();
   await currentTask.reload({
