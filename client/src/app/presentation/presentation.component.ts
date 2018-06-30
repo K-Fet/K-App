94b131836c81a51bbacbcc9450320464a379afb0
Corentin GIRAUD <corentin.giraud@insa-lyon.fr>
2018-06-30 13:27:56 +0200

    Forget one thing

--- client/src/app/presentation/presentation.component.ts
+++ client/src/app/presentation/presentation.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
   styleUrls: ['./presentation.component.scss'],
 })
-export class PresentationComponent implements OnInit {
+export class PresentationComponent {
 }
