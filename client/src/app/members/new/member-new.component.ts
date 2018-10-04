94b3c4acf25f97bc4a53704a4ba14a571e49ed1a
Corentin GIRAUD <corentin@copromatic.com>
2018-10-04 18:13:22 -0400

    Finish new component

--- client/src/app/members/new/member-new.component.ts
+++ client/src/app/members/new/member-new.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MemberService, ToasterService } from '../../_services';
 import { Router } from '@angular/router';
-import { Member } from '../../_models';
+import { Member, AVAILABLE_SCHOOLS } from '../../_models';
 import { FormArray } from '@angular/forms/src/model';
 import { ValidateCheckbox } from '../../_validators/checkbox.validator';
 
@@ -11,15 +11,12 @@ import { ValidateCheckbox } from '../../_validators/checkbox.validator';
 })
 
 export class MemberNewComponent implements OnInit {
-  firstName: string;
-  lastName: string;
-  school: string;
-  code: number;
-
   formArray: FormArray;
   memberFormGroup: FormGroup;
-  codeFormGroup: FormGroup;
+  barmanFormGroup: FormGroup;
   form: FormGroup;
+
+  availableSchools = AVAILABLE_SCHOOLS;
 
   constructor(
     private memberService: MemberService,
@@ -28,31 +25,49 @@ export class MemberNewComponent implements OnInit {
     private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
+    this.initValidation();
+  }
+
+  initForm(): void {
     this.memberFormGroup = this.formBuilder.group({
-      lastNameFormControl: ['', Validators.required],
-      firstNameFormControl: ['', Validators.required],
-      schoolFormControl: ['', Validators.required],
-      statutFormControl: ['', ValidateCheckbox],
-      riFormControl: ['', ValidateCheckbox],
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      school: ['', Validators.required],
     });
-    this.codeFormGroup = this.formBuilder.group({
-      codeFormControl: ['', [Validators.required, Validators.pattern(/^[0-9]{4,}$/)]],
+    this.barmanFormGroup = this.formBuilder.group({
+      statut: [true, ValidateCheckbox],
+      ri: [true, ValidateCheckbox],
+      code: ['', [Validators.required, Validators.pattern(/^[0-9]{4,}$/)]],
     });
     this.formArray = this.formBuilder.array([
       this.memberFormGroup,
-      this.codeFormGroup,
+      this.barmanFormGroup,
     ]);
     this.form = this.formBuilder.group({
       formArray: this.formArray,
     });
   }
 
+  initValidation(): void {
+    this.memberFormGroup.controls.lastName.valueChanges.subscribe((value) => {
+      this.memberFormGroup.controls.lastName.setValue(this.nameFormatter(value), { emitEvent: false });
+    });
+    this.memberFormGroup.controls.firstName.valueChanges.subscribe((value) => {
+      this.memberFormGroup.controls.firstName.setValue(this.nameFormatter(value), { emitEvent: false });
+    });
+  }
+
+  nameFormatter(name: string): string {
+    const newName = !name ? name :
+      name.replace(/[\wÀ-ÿ]+(\S&-)*/g, txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase())
+      .replace(/[`~!@#$%^&*()_|+\=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+    return newName;
+  }
+
   add(): void {
-    const member = new Member();
-    member.firstName = this.firstName;
-    member.lastName = this.lastName;
-    member.school = this.school;
-    this.memberService.create(member, this.code).subscribe(() => {
+    const member = new Member(...this.memberFormGroup.value);
+    this.memberService.create(member, this.barmanFormGroup.controls.code.value).subscribe(() => {
       this.toasterService.showToaster('Adhérent créé');
       this.router.navigate(['/members']);
     });
