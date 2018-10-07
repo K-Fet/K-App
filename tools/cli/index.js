382dfc4e76f239faa83ad94d182be4cb93de1df1
Corentin GIRAUD <corentin@copromatic.com>
2018-10-06 23:34:55 -0400

    Implement migrate down cli action

--- tools/cli/index.js
+++ tools/cli/index.js
@@ -26,7 +26,14 @@ const actions = [
   },
   {
     key: 'migrate',
-    description: 'Migrate the current database to the new version',
+    description: 'Migrate the current database to the new version.'
+      + 'Will execute all pending / not yet executed migration scripts'
+      + 'localized in `./migrate/scripts/`',
+    noGui: true,
+  },
+  {
+    key: 'migrate down',
+    description: 'Revert the last executed migration script.',
     noGui: true,
   },
   {
