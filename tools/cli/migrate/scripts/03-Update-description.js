f5ddec2768f93ca3d397b609b328c6172f403df3
Corentin GIRAUD <corentin@copromatic.com>
2018-10-07 15:58:43 -0400

    Update scripts

--- tools/cli/migrate/scripts/03-Update-description.js
+++ tools/cli/migrate/scripts/03-Update-description.js
@@ -1,19 +1,19 @@
 /* eslint-disable require-jsdoc,no-unused-vars */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Kommissions', 'description', {
+    await queryInterface.changeColumn('kommissions', 'description', {
       type: Sequelize.TEXT,
     });
-    await queryInterface.changeColumn('Roles', 'description', {
+    await queryInterface.changeColumn('roles', 'description', {
       type: Sequelize.TEXT,
     });
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.changeColumn('Kommissions', 'description', {
+    await queryInterface.changeColumn('kommissions', 'description', {
       type: Sequelize.STRING,
     });
-    await queryInterface.changeColumn('Roles', 'description', {
+    await queryInterface.changeColumn('roles', 'description', {
       type: Sequelize.STRING,
     });
   },
--- tools/cli/migrate/scripts/04-Add-leaveAt.js
+++ tools/cli/migrate/scripts/04-Add-leaveAt.js
@@ -1,25 +1,66 @@
+const dotenv = require('dotenv');
+const { Barman } = require('../../../../server/app/models');
+const sequelize = require('../../../../server/bootstrap/sequelize');
+const config = require('../../../../server/bootstrap/config');
+
+async function init() {
+  dotenv.config();
+  config.start();
+  await sequelize.start();
+}
+
+async function setLeaveAtForOldBarmen() {
+  const oldBarmen = await Barman.findAll({
+    where: {
+      active: false,
+    },
+  });
+  const promises = [];
+  oldBarmen.forEach((oldBarman) => {
+    const newOldBarman = oldBarman;
+    newOldBarman.leaveAt = new Date('2018-07-31');
+    promises.push(newOldBarman.save());
+  });
+  await Promise.all(promises);
+}
+
+async function setActiveForBarmen() {
+  const barmen = await Barman.findAll();
+  const promises = [];
+  barmen.forEach((barman) => {
+    const newBarman = barman;
+    newBarman.active = !barman.leaveAt;
+    promises.push(newBarman.save());
+  });
+  await Promise.all(promises);
+}
+
 /* eslint-disable require-jsdoc,no-unused-vars */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const barmans = await queryInterface.describeTable('Barmans');
+    const barmans = await queryInterface.describeTable('barmans');
+    await init();
     if (!barmans.leaveAt) {
-      await queryInterface.addColumn('Barmans', 'leaveAt', Sequelize.DATEONLY);
+      await queryInterface.addColumn('barmans', 'leaveAt', Sequelize.DATEONLY);
+      await setLeaveAtForOldBarmen();
     }
     if (barmans.active) {
-      await queryInterface.removeColumn('Barmans', 'active');
+      await queryInterface.removeColumn('barmans', 'active');
     }
   },
 
   async down(queryInterface, Sequelize) {
-    const barmans = await queryInterface.describeTable('Barmans');
-    if (barmans.leaveAt) {
-      await queryInterface.removeColumn('Barmans', 'leaveAt');
-    }
+    const barmans = await queryInterface.describeTable('barmans');
+    await init();
     if (!barmans.active) {
-      await queryInterface.addColumn('Barmans', 'active', Sequelize.BOOLEAN, {
+      await queryInterface.addColumn('barmans', 'active', Sequelize.BOOLEAN, {
         allowNull: false,
         default: true,
       });
+      await setActiveForBarmen();
+    }
+    if (barmans.leaveAt) {
+      await queryInterface.removeColumn('barmans', 'leaveAt');
     }
   },
 };
