cfd6908d33d94b5eb01f3f67c0477ab13f3067ac
Marc-Antoine FERNANDES <marcantoinefernandes@gmail.com>
2018-12-15 17:39:14 +0100

    Fix login and remove route tracing
    Signed-off-by: Marc-Antoine FERNANDES <marcantoinefernandes@gmail.com>

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -65,7 +65,6 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes, {
       preloadingStrategy: PreloadAllModules,
-      enableTracing: true,
     }),
   ],
   exports: [RouterModule],
