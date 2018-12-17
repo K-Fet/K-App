c3e116671dbc849d947e7773f55c2162d77e5dc1
Marc-Antoine Fernandes <marcantoinefernandes@gmail.com>
2018-12-17 13:14:13 +0100

    Remove switch

--- server/moleculer-app/services/contact.service.js
+++ server/moleculer-app/services/contact.service.js
@@ -4,6 +4,13 @@ const recaptchaService = require('../../app/services/recaptcha-service');
 const TRANSLATIONS = require('../../resources/contact-form-field-translations');
 const { flatten } = require('../../utils');
 
+const CONTACTS = {
+  concert: { title: 'Concert' },
+  event: { title: 'Évenement | soirée' },
+  lost: { title: 'Objet perdu' },
+  website: { title: 'Problème avec la K-App' },
+};
+
 module.exports = {
   name: 'contact',
 
@@ -21,27 +28,11 @@ module.exports = {
         await recaptchaService.recaptchaVerification(ctx.params.token);
         const { contactFormName, values } = ctx.params;
 
-        let formName = '';
-        switch (contactFormName) {
-          case 'concert':
-            formName = 'Concert';
-            break;
-          case 'event':
-            formName = 'Évenement | soirée';
-            break;
-          case 'lost':
-            formName = 'Objet perdu';
-            break;
-          case 'website':
-            formName = 'Problème avec la K-App';
-            break;
-          default:
-            return;
-        }
+        const form = CONTACTS[contactFormName];
 
         const email = conf.get(`mail:contact:${contactFormName}`);
         const data = {
-          formName, values: flatten(values), email, TRANSLATIONS,
+          form, values: flatten(values), email, TRANSLATIONS,
         };
 
         await this.sendMail(ctx, email, data);
