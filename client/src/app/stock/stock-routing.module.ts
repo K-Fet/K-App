aa88cf1188e49ad0a96e39d6c62739ce6624c0ec
mrauturier <maxime.rauturier@insa-lyon.fr>
2019-01-07 13:30:15 +0100

    add first version of stepper for adding an order

--- client/src/app/stock/stock-routing.module.ts
+++ client/src/app/stock/stock-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {StockViewComponent} from "./stock-view/stock-view.component";
 import {StockMenuComponent} from "./stock-menu/stock-menu.component";
+import {AddOrderViewComponent} from "./add-order-view/add-order-view.component";
 
 const routes: Routes = [
-  { path: 'view', component: StockViewComponent},
-  { path: '', component: StockMenuComponent},
+  { path: 'stock/view', component: StockViewComponent},
+  { path: 'stock', component: StockMenuComponent},
+  { path:'stock/orders/add', component: AddOrderViewComponent}
 ];
 
 @NgModule({
--- client/src/app/stock/stock.module.ts
+++ client/src/app/stock/stock.module.ts
@@ -4,9 +4,10 @@ import { StockMenuComponent } from './stock-menu/stock-menu.component';
 
 import {SharedModule} from "../shared/shared.module";
 import { StockRoutingModule } from './stock-routing.module';
+import { AddOrderViewComponent } from './add-order-view/add-order-view.component';
 
 @NgModule({
-  declarations: [StockViewComponent, StockMenuComponent],
+  declarations: [StockViewComponent, StockMenuComponent, AddOrderViewComponent],
   imports: [
     SharedModule,
     StockRoutingModule,
