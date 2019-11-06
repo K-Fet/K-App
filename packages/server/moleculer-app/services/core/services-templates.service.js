7b9f283c5a1318040dc343a5b2909071ed52c9ea
Marc-Antoine Fernandes <marcantoinefernandes@gmail.com>
2019-11-06 18:32:09 +0100

    Add services and services templates in moleculer

---
+++ packages/server/moleculer-app/services/core/services-templates.service.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
+const JoiDbActionsMixin = require('../../mixins/joi-db-actions.mixin');
+const DbMixin = require('../../mixins/db-service.mixin');
+const { MONGO_ID, MONGOOSE_INTERNALS, createSchema } = require('../../../utils');
+
+const getMongooseDayFields = (prefix = '') => ({
+  [`${prefix}Day`]: {
+    type: Number, required: true, min: 1, max: 7,
+  },
+  [`${prefix}Hours`]: {
+    type: Number, required: true, min: 0, max: 23,
+  },
+  [`${prefix}Minutes`]: {
+    type: Number, required: true, min: 0, max: 59,
+  },
+});
+
+const getJoiDayFields = (prefix = '') => ({
+  [`${prefix}Day`]: Joi.number().integer().min(1).max(7)
+    .required(),
+  [`${prefix}Hours`]: Joi.number().integer().min(0).max(23)
+    .required(),
+  [`${prefix}Minutes`]: Joi.number().integer().min(0).max(59)
+    .required(),
+});
+
+const model = {
+  mongoose: mongoose.model('Members', createSchema({
+    name: { type: Date, required: true },
+    services: [{
+      nbMax: { type: Number, default: 3 },
+      ...getMongooseDayFields('start'),
+      ...getMongooseDayFields('end'),
+    }],
+  }, { timestamp: true }, { textIndex: ['name'] })),
+  joi: Joi.object({
+    _id: MONGO_ID.strip(), // Remove _id from the object
+    ...MONGOOSE_INTERNALS,
+    name: Joi.string().trim().required(),
+    services: Joi.array().items(Joi.object({
+      nbMax: Joi.number().integer().min(1),
+      ...getJoiDayFields('start'),
+      ...getJoiDayFields('end'),
+    }).min(1).required()),
+  }),
+};
+
+module.exports = {
+  name: 'core.services-templates',
+  version: 1,
+  mixins: [
+    JoiDbActionsMixin(model.joi),
+    DbMixin(model.mongoose),
+  ],
+
+  settings: {
+    rest: 'v1/services-templates/',
+  },
+
+  actions: {},
+};
---
+++ packages/server/moleculer-app/services/core/services.service.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
+const JoiDbActionsMixin = require('../../mixins/joi-db-actions.mixin');
+const DbMixin = require('../../mixins/db-service.mixin');
+const { MONGO_ID, createSchema } = require('../../../utils');
+
+const { ObjectID } = mongoose.Schema.Types;
+
+const model = {
+  mongoose: mongoose.model('Members', createSchema({
+    startAt: { type: Date, required: true, index: true },
+    endAt: { type: Date, required: true, index: true },
+    nbMax: { type: Number, min: 1, default: 1 },
+    barmen: [{ type: ObjectID, index: true }],
+  })),
+  joi: Joi.object({
+    _id: MONGO_ID.strip(), // Remove _id from the object
+    __v: Joi.number().integer(),
+    startAt: Joi.date().iso().required(),
+    endAt: Joi.date().iso().min(Joi.ref('startAt')).required(),
+    barmen: Joi.array().items(MONGO_ID),
+    nbMax: Joi.number().integer().min(1),
+  }),
+};
+
+module.exports = {
+  name: 'core.services',
+  version: 1,
+  mixins: [
+    JoiDbActionsMixin(model.joi),
+    DbMixin(model.mongoose),
+  ],
+
+  settings: {
+    rest: 'v1/services/',
+  },
+
+  actions: {},
+};
