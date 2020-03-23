af5921cdefde582064e454e22180cff6d36b5244
Marc-Antoine Fernandes <marcantoinefernandes@gmail.com>
2020-03-23 09:30:31 +0100

    Add query params in members

--- packages/client/src/app/members/list/list.component.ts
+++ packages/client/src/app/members/list/list.component.ts
@@ -3,7 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MediaObserver } from '@angular/flex-layout';
-import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, first, tap } from 'rxjs/operators';
 import { fromEvent, merge } from 'rxjs';
 import { Member } from '../member.model';
 import { MoleculerDataSource } from '../../shared/utils/moleculer-data-source';
@@ -12,6 +12,7 @@ import { ToasterService } from '../../core/services/toaster.service';
 import { CURRENT_SCHOOL_YEAR } from '../../constants';
 import { RegisterMemberDialogComponent } from '../../shared/dialogs/register-member/register-member-dialog.component';
 import { NgxPermissionsService } from 'ngx-permissions';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './list.component.html',
@@ -28,58 +29,85 @@ export class ListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
-  @ViewChild('input', { static: true }) input: ElementRef;
+  @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
 
   constructor(private membersService: MembersService,
-              private toasterService: ToasterService,
-              private mediaObserver: MediaObserver,
-              private ngxPermissionsService: NgxPermissionsService,
-              private dialog: MatDialog) {
+    private toasterService: ToasterService,
+    private mediaObserver: MediaObserver,
+    private ngxPermissionsService: NgxPermissionsService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private dialog: MatDialog) {
   }
 
-  async ngOnInit() {
+  async ngOnInit(): Promise<void> {
     if (this.mediaObserver.isActive('sm') || this.mediaObserver.isActive('xs')) {
       this.displayedColumns = ['lastName', 'firstName', 'actions'];
     }
 
     this.dataSource = new MoleculerDataSource<Member, MembersOptions>(this.membersService);
 
-    // Load and save initial count as number of registered members
-    await this.dataSource.load({ sort: '-updatedAt' });
-    this.totalRegistered = this.dataSource.total;
+    // Load state from query params
+    this.route.queryParams.pipe(first())
+      .subscribe((value) => {
+        this.paginator.pageSize = +value['pageSize'];
+        this.paginator.pageIndex = +value['page'] - 1;
 
-    // Refresh list on change
-    this.membersService.refresh$.subscribe(() => this.loadMembersPage());
+        const [, direction, sort] = /^(-)?(\w+)$/.exec(value['sort'] || '') || [undefined, '-', 'updatedAt'];
+        this.sort.direction = direction ? 'desc' : 'asc';
+        this.sort.active = sort;
+
+        this.searchInput.nativeElement.value = value['search'] || '';
+
+        if (value['active'] === 'true') {
+          this.selectedStatus = 'active';
+        } else if (value['inactive'] === 'true') {
+          this.selectedStatus = 'inactive';
+        } else {
+          this.selectedStatus = '';
+        }
+      });
+
+    // Load and save initial count as number of registered members
+    await this.loadMembersPage({ skipRouteQueryParams: true });
+    this.totalRegistered = this.dataSource.total;
   }
 
   ngAfterViewInit(): void {
     // Server-side search
-    fromEvent(this.input.nativeElement, 'keyup').pipe(
+    const search = fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
       debounceTime(300),
       distinctUntilChanged(),
-      tap(() => {
-        this.paginator.pageIndex = 0;
-        this.loadMembersPage();
-      }),
-    ).subscribe();
+      tap(() => this.paginator.pageIndex = 0),
+    );
 
-    // Paginator and Sort
-    // reset the paginator after sorting
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
-    merge(this.sort.sortChange, this.paginator.page).pipe(tap(() => this.loadMembersPage())).subscribe();
+    // Reset the paginator after sorting
+    const sort = this.sort.sortChange.pipe(tap(() => this.paginator.pageIndex = 0));
+
+    // Merge everything to reload members
+    merge(search, sort, this.paginator.page, this.membersService.refresh$).subscribe(() => this.loadMembersPage());
   }
 
-  async loadMembersPage() {
+  async loadMembersPage({ skipRouteQueryParams }: { skipRouteQueryParams?: boolean } = {}): Promise<void> {
     const options = {
       pageSize: this.paginator.pageSize,
       page: this.paginator.pageIndex + 1,
-      search: this.input.nativeElement.value,
+      search: this.searchInput.nativeElement.value,
       sort: this.sort.active && `${this.sort.direction === 'desc' ? '-' : ''}${this.sort.active}`,
       active: this.selectedStatus === 'active',
       inactive: this.selectedStatus === 'inactive',
     };
 
     await this.dataSource.load(options);
+
+    if (!skipRouteQueryParams) {
+      await this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: options,
+        queryParamsHandling: 'merge',
+      });
+    }
+
     // If initial settings, update totalRegistered
     if (!options.search && options.active) this.totalRegistered = this.dataSource.total;
   }
@@ -88,7 +116,7 @@ export class ListComponent implements OnInit, AfterViewInit {
     return !!member.registrations.find(r => r.year === CURRENT_SCHOOL_YEAR);
   }
 
-  async register(member: Member) {
+  async register(member: Member): Promise<void> {
     const dialogRef = this.dialog.open(RegisterMemberDialogComponent, {
       width: '350px',
       data: member,
