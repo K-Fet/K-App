cb2a975c265776a20f1167cf7e4035d186ff4d93
Adrien Jaillet <adrien.jaillet@insa-lyon.fr>
2020-03-30 15:59:56 +0200

    Interface invoice tools done

---
+++ packages/client/src/app/inventory-management/invoice-tool/invoice-service/invoice.service.ts
@@ -0,0 +1,25 @@
+import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+
+export class InvoiceService {
+
+    invoicesSubject = new Subject<any[]>();
+    private invoices = [];
+
+    emitAppareilSubject() {
+        this.invoicesSubject.next(this.invoices.slice());
+    }
+
+    addInvoice(file : File) {
+        const invoiceObject = file;
+        this.invoices.push(invoiceObject);
+        this.emitAppareilSubject();
+      }
+    removeInvoice() {
+        this.invoices.pop();
+        this.emitAppareilSubject();
+      }
+
+}
