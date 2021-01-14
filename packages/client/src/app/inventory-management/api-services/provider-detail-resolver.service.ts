5394f43bff7eacbd15c8cb3f5a8196c157cfd218
Adrien Jaillet <61200077+a-jaillet@users.noreply.github.com>
2021-01-14 14:40:36 +0100

    Update packages/client/src/app/inventory-management/api-services/provider-detail-resolver.service.ts
    Co-authored-by: Marc-Antoine <marcantoinefernandes@gmail.com>

--- packages/client/src/app/inventory-management/api-services/provider-detail-resolver.service.ts
+++ packages/client/src/app/inventory-management/api-services/provider-detail-resolver.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, Router, } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
 import { Provider } from '../providers/provider.model';
 import { ProvidersService } from './providers.service';
 
